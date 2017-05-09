5723ccfcf2b03aa00c8985dbc6a23956a7c26e9b
Sandro Mancuso <sandromancuso@yahoo.com>
2017-05-09 19:12:14 +0100

    Used JUnitParams for unit tests

--- src/test/java/com/codurance/RomanNumeralConverterShould.java
+++ src/test/java/com/codurance/RomanNumeralConverterShould.java
@@ -1,28 +1,36 @@
 package com.codurance;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static com.codurance.RomanNumeralConverter.romanFor;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
 public class RomanNumeralConverterShould {
 
-    @Test public void
-    generate_a_roman_numeral_for_a_given_arabic_number() {
-        assertThat(romanFor(1), is("I"));
-        assertThat(romanFor(2), is("II"));
-        assertThat(romanFor(3), is("III"));
-        assertThat(romanFor(4), is("IV"));
-        assertThat(romanFor(5), is("V"));
-        assertThat(romanFor(7), is("VII"));
-        assertThat(romanFor(9), is("IX"));
-        assertThat(romanFor(10), is("X"));
-        assertThat(romanFor(18), is("XVIII"));
-        assertThat(romanFor(30), is("XXX"));
+    @Test
+    @Parameters({
+            "1, I",
+            "2, II",
+            "3, III",
+            "4, IV",
+            "5, V",
+            "7, VII",
+            "9, IX",
+            "10, X",
+            "17, XVII",
+            "30, XXX",
+            "38, XXXVIII",
 
-        assertThat(romanFor(2494), is("MMCDXCIV"));
-        assertThat(romanFor(3548), is("MMMDXLVIII"));
+            "479, CDLXXIX",
+            "3999, MMMCMXCIX"
+    }) public void
+    convert_arabic_numbers_into_their_respective_roman_numeral(int arabic, String roman) {
+        assertThat(romanFor(arabic), is(roman));
     }
 
 }
